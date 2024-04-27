461b098c3f6af56759d928d734b36e792ee571bb
Phil Steitz <phil.steitz@gmail.com>
2024-04-27 15:45:44 -0700

    Allow missing headers, non-pipe delimiters.

--- runner/greppy.py
+++ runner/greppy.py
@@ -25,8 +25,8 @@ def parse_rules(config_file: str, fields: Dict[str, int]) -> str:
         negate = False
         for line in lines:
             line = line.strip()
-            if len(line) == 0 or line.startswith('#'):
-                continue  # Skip comment lines and blank lines
+            if len(line) == 0 or line.startswith('#') or line.startswith('!'):
+                continue  # Skip comment lines, blank lines and directives
             if line in ['OR', 'AND']:
                 if line == 'OR':
                     operator = '||'
@@ -57,12 +57,13 @@ def parse_rules(config_file: str, fields: Dict[str, int]) -> str:
             # Get the value
             value = parts[value_index].strip()
             # Check if value is a list.  If so, generate a regex to match any value in the list.
-            # Someting like this: '~/^(231|117|21|7) $/'
+            # Someting like this: '~//^[ ]*\"?(231|117|21|7)"?[ ]*$/'
+            # ignoring leading and trailing spaces and optional quotes
             if value.startswith('[') and value.endswith(']'):
                 value = value[1:-1].split(',')
                 value = [v.strip() for v in value]
                 value = '|'.join(value)
-                value = f"/^({value}) $/"
+                value = f"/^[ ]*\"?({value})\"?[ ]*$/"
 
             # Add the clause to the components list
             components.append(
@@ -96,22 +97,29 @@ def parse_rules(config_file: str, fields: Dict[str, int]) -> str:
     return match
 
 
-def generate_awk_script(match: str, fields: Dict[str, int]) -> str:
+def generate_awk_script(match: str, fields: Dict[str, int], field_separator="|", has_header=True) -> str:
     """
     Generate the awk script, using the match string.
         args:
             match: The match string generated from the rules in the config file.
             fields: A dictionary of field names and their index in the csv file.
+            field_separator: The separator used in the csv file. Default is '|'.
         returns:
             A string that is the awk script that can be used to search the csv files.
     """
-    # Generate the awk script.
-    # In BEGIN block, Set the delimiter to | and print header line with field names.
-    # For each line, if the line matches the match string, print the line.
-    # Generate header line from fields dictionary
-    header = '|'.join(fields.keys())
-    awk_script = f'BEGIN {{ FS="|"; print "{header}" }}\n'
-    awk_script += f'NR > 1 && {match}  {{ print $0 }}\n'
+    # If input file has no headers, generate a header line from the fields dictionary
+    # Also, match all lines in the file.
+    if not has_header:
+        header = field_separator.join(fields.keys())
+        # Strip off the last field_separator
+        header = header[:-1]
+        awk_script = f'BEGIN {{ FS="{field_separator}"; print "{header}" }}\n'
+        awk_script += f'{match}  {{ print $0 }}\n'
+    else:
+        # If input file has headers, print the first line and then match the rest of the lines.
+        awk_script = f'BEGIN {{ FS="{field_separator}"}}\n'
+        awk_script += 'NR == 1 { print $0 }\n'
+        awk_script += f'NR > 1 && {match}  {{ print $0 }}\n'
     return awk_script
 
 
@@ -133,14 +141,15 @@ def get_file_spec(config_file: str) -> Tuple[str, str]:
         return 'file', first_line
 
 
-def get_fields(path, path_type) -> dict:
-    """ Get the fields from the first line of the file_spec or the first csv file in the directory.
+def get_fields(path: str, path_type: str, field_separator: str) -> dict:
+    """ 
+     Get the fields from the first line of the file_spec or the first csv file in the directory.
      Return a dictionary of field names and their index in the csv file.
      Add an empty string key with value 0 for full record match.
         args:
             path: The file or directory to be searched.
             path_type: 'dir' if path is a directory, 'file' if path is a file.
-
+            field_separator: The separator used in the csv file.
         returns:
             A dictionary of field names and their index in the csv file.
      """
@@ -149,20 +158,49 @@ def get_fields(path, path_type) -> dict:
         for file in file_list:
             if file.endswith('.csv'):
                 with open(file, 'r', encoding='utf-8') as f:
-                    fields = f.readline().strip().split('|')
+                    fields = f.readline().strip().split(field_separator)
                     ret = {fields[i].strip(): i +
                            1 for i in range(len(fields))}
+                    break
     else:
         with open(path, 'r', encoding='utf-8') as f:
-            fields = f.readline().strip().split('|')
+            fields = f.readline().strip().split(field_separator)
             ret = {fields[i].strip(): i + 1 for i in range(len(fields))}
     # Add {"": 0} to the fields dictionary for full record match
     ret[""] = 0
     return ret
 
 
+def process_directives(config_file: str) -> Tuple[str, dict]:
+    """Process directives in the config file.
+    args:
+        config_file: The name of the config file that contains the match rules.
+    returns:
+        A tuple containing the field separator and a list of column names.
+        If !FIELDS directive is found, the column names are taken from the directive, otherwise an empty list is returned.
+        If !SEPARATOR directive is found, the field separator is taken from the directive, otherwise '|' is returned.
+    """
+    field_separator = '|'
+    fields = {}
+    with open(config_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('!FIELDS'):
+                # Strip off the "!FIELDS" directive
+                line = line[7:].strip()
+                # Strip the [] from the line and split on commas
+                line = line[1:-1]
+                field_list = line.split(',')
+                fields = {name.strip(): i + 1 for i, name in enumerate(
+                    field_list)}
+                fields[""] = 0
+            if line.startswith('!SEPARATOR'):
+                field_separator = line.split(' ')[1].strip()
+    return field_separator, fields
+
+
 def main():
-    """Main function for the greppy utility."""
+    """Main function."""
     # Parse command line arguments.  Expecting a single argument, the greppy match rules file, defaulting to greppy.txt
     parser = argparse.ArgumentParser(
         description='Greppy: A simple grep-like utility')
@@ -173,8 +211,14 @@ def main():
     # Get the file spec from the config file and determine if it is a file or a directory
     path_type, file_spec = get_file_spec(args.config_file)
 
+    # Process directives - !HEADER and !SEPARATOR
+    field_separator, fields = process_directives(args.config_file)
+
     # Get the fields from the first line of the file_spec or the first csv file in the directory
-    fields = get_fields(file_spec, path_type)
+    # if column names are not provided in directive.
+    has_fields = len(fields) == 0  # has_fields means input files have headers
+    if has_fields:
+        fields = get_fields(file_spec, path_type, field_separator)
 
     # Parse the rules from the config file
     match = parse_rules(args.config_file, fields)
@@ -183,14 +227,14 @@ def main():
     # and removing special characters.
     output_name = file_spec + '_' + args.config_file
     output_name = output_name.replace('/', '_').replace(
-        '.', '_').replace(' ', '_').replace('csv', '').replace('txt', '').replace('__', '_').replace('\\', '_').replace('C:', '')
+        '.', '_').replace(' ', '_').replace('csv', '').replace('txt', '').replace('__', '_').replace('\\', '_').replace('C:', '').replace('-', '_')
     script_name = output_name + '.awk'
 
     # Generate and save the awk script
-    awk_script = generate_awk_script(match, fields)
-    p = Path(__file__).with_name(script_name)
+    awk_script = generate_awk_script(
+        match, fields, field_separator, has_fields)
 
-    with p.open('w', encoding='utf-8') as f:
+    with open(script_name, 'w', encoding='utf-8') as f:
         f.write(awk_script)
 
     # Generate a list of files to process.  If the file_spec is a file, just process that file.
@@ -204,6 +248,7 @@ def main():
     # Also pipe the results to a file with the same name as the file_spec with a .csv extension.
     for _, file in enumerate(file_list):
         print(f"Results for {file}")
+        sys.stdout.flush()
         p = Path(__file__).with_name(output_name + '.csv')
         with p.open('wb') as out:
             with subprocess.Popen(
@@ -212,6 +257,7 @@ def main():
                     sys.stdout.buffer.writelines([line.encode()])
                     out.writelines([line.encode()])
                 sys.stdout.flush()
+                sys.stderr.flush()
                 out.flush()
                 proc.wait()
 
--- tests/test_greppy.py
+++ tests/test_greppy.py
@@ -16,24 +16,24 @@ def test_get_fields():
     """Test get_fields function"""
     expected = {'ProductId': 1, 'ProductPrice': 2,
                 'ProductDescription': 3, 'ProductCategory': 4, '': 0}
-    assert get_fields('./test_files/test.csv', 'file') == expected
-    assert get_fields('./test_files', 'dir') == expected
+    assert get_fields('./test_files/test.csv', 'file', '|') == expected
+    assert get_fields('./test_files', 'dir', '|') == expected
 
 
 def test_rules_simple_grep():
     """Test parse_rules function on simple grep"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     assert parse_rules('./test_pwd.txt', fields) == "$0 ~ /crumpet/"
 
 
 def test_generate_awk_script_simple_grep():
     """Test generate_awk_script function on simple grep"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     match = parse_rules('./test_pwd.txt', fields)
     script_lines = generate_awk_script(match, fields).split('\n')
-    assert script_lines[
-        0] == 'BEGIN { FS="|"; print "ProductId|ProductPrice|ProductDescription|ProductCategory|" }'
-    assert script_lines[1] == 'NR > 1 && $0 ~ /crumpet/  { print $0 }'
+    assert script_lines[0] == 'BEGIN { FS="|"}'
+    assert script_lines[1] == 'NR == 1 { print $0 }'
+    assert script_lines[2] == 'NR > 1 && $0 ~ /crumpet/  { print $0 }'
 
 
 def test_main_simple_grep(capsys):
@@ -42,7 +42,7 @@ def test_main_simple_grep(capsys):
     main()
     captured = capsys.readouterr().out.split('\n')
     assert captured[0] == 'Results for ./test_files/test.csv'
-    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory|'
+    assert captured[1] == 'ProductId | ProductPrice | ProductDescription | ProductCategory'
     assert captured[2] == '1121 | 7.50 | fancy crumpets | grocery'
     assert captured[3] == '1122 | 3.00 | plain crumpets | grocery'
     assert captured[4] == '1125 | 1.00 | sugar crumpets | "snacks"'
@@ -50,19 +50,19 @@ def test_main_simple_grep(capsys):
 
 def test_rules_column_search():
     """Test parse_rules function on simple grep"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     assert parse_rules('./test_column_search.txt',
                        fields) == '$1 ~ /^[ ]*"?1128"?[ ]*$/'
 
 
 def test_generate_awk_script_column_search():
     """Test generate_awk_script function on simple grep"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     match = parse_rules('./test_column_search.txt', fields)
     script_lines = generate_awk_script(match, fields).split('\n')
-    assert script_lines[
-        0] == 'BEGIN { FS="|"; print "ProductId|ProductPrice|ProductDescription|ProductCategory|" }'
-    assert script_lines[1] == 'NR > 1 && $1 ~ /^[ ]*"?1128"?[ ]*$/  { print $0 }'
+    assert script_lines[0] == 'BEGIN { FS="|"}'
+    assert script_lines[1] == 'NR == 1 { print $0 }'
+    assert script_lines[2] == 'NR > 1 && $1 ~ /^[ ]*"?1128"?[ ]*$/  { print $0 }'
 
 
 def test_main_column_search(capsys):
@@ -71,26 +71,26 @@ def test_main_column_search(capsys):
     main()
     captured = capsys.readouterr().out.split('\n')
     assert captured[0] == 'Results for test_files/test.csv'
-    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory|'
+    assert captured[1] == 'ProductId | ProductPrice | ProductDescription | ProductCategory'
     assert captured[2] == '1128 || "peanut butter dog treats" | pets'
 
 
 def test_parse_rules_not_or():
     """Test parse_rules function on negated disjunction"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     assert parse_rules('./test_multi_file.txt',
-                       fields) == "!($1 ~ /^(1123|1128|1129) $/ || $3 ~ /crumpets/)"
+                       fields) == "!($1 ~ /^[ ]*\"?(1123|1128|1129)\"?[ ]*$/ || $3 ~ /crumpets/)"
 
 
 def test_generate_awk_script_not_or():
     """Test generate_awk_script function on negated disjunction"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     match = parse_rules('./test_multi_file.txt', fields)
     script_lines = generate_awk_script(match, fields).split('\n')
+    assert script_lines[0] == 'BEGIN { FS="|"}'
+    assert script_lines[1] == 'NR == 1 { print $0 }'
     assert script_lines[
-        0] == 'BEGIN { FS="|"; print "ProductId|ProductPrice|ProductDescription|ProductCategory|" }'
-    assert script_lines[
-        1] == 'NR > 1 && !($1 ~ /^(1123|1128|1129) $/ || $3 ~ /crumpets/)  { print $0 }'
+        2] == 'NR > 1 && !($1 ~ /^[ ]*\"?(1123|1128|1129)\"?[ ]*$/ || $3 ~ /crumpets/)  { print $0 }'
 
 
 def test_main_not_or(capsys):
@@ -99,7 +99,7 @@ def test_main_not_or(capsys):
     main()
     captured = capsys.readouterr().out.split('\n')
     assert captured[0] == 'Results for ./test_files/test.csv'
-    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory|'
+    assert captured[1] == 'ProductId | ProductPrice | ProductDescription | ProductCategory'
     assert captured[2] == '1124| 2.00 | Salty jerky treads | pets'
     assert captured[3] == '1126 | 1.00 | plain peanut butter | grocery'
     assert captured[4] == '1127 | 2.00 | peanut brittle | snacks'
@@ -109,20 +109,20 @@ def test_main_not_or(capsys):
 
 def test_parse_rules_not_and():
     """Test parse_rules function on AND search with negation."""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     assert parse_rules('./test_and.txt',
                        fields) == '$3 ~ /nut/ && $4 !~ /^[ ]*"?snacks"?[ ]*$/'
 
 
 def test_generate_awk_script_not_and():
     """Test generate_awk_script function on AND search with negation."""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     match = parse_rules('./test_and.txt', fields)
     script_lines = generate_awk_script(match, fields).split('\n')
+    assert script_lines[0] == 'BEGIN { FS="|"}'
+    assert script_lines[1] == 'NR == 1 { print $0 }'
     assert script_lines[
-        0] == 'BEGIN { FS="|"; print "ProductId|ProductPrice|ProductDescription|ProductCategory|" }'
-    assert script_lines[
-        1] == 'NR > 1 && $3 ~ /nut/ && $4 !~ /^[ ]*"?snacks"?[ ]*$/  { print $0 }'
+        2] == 'NR > 1 && $3 ~ /nut/ && $4 !~ /^[ ]*"?snacks"?[ ]*$/  { print $0 }'
 
 
 def test_main_not_and(capsys):
@@ -133,7 +133,7 @@ def test_main_not_and(capsys):
     captured = capsys.readouterr().out.split('\n')
     print(captured)
     assert captured[0] == 'Results for test_files/test.csv'
-    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory|'
+    assert captured[1] == 'ProductId | ProductPrice | ProductDescription | ProductCategory'
     assert captured[2] == '1126 | 1.00 | plain peanut butter | grocery'
     assert captured[3] == '1128 || "peanut butter dog treats" | pets'
     assert captured[4] == '1129 | 3.00 | peanuts ||'
@@ -141,20 +141,19 @@ def test_main_not_and(capsys):
 
 def test_parse_rules_value_range():
     """Test parse_rules function on value range"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     assert parse_rules('./test_value_range.txt',
                        fields) == '$2 >= 2 && $2 < 5'
 
 
 def test_generate_awk_script_value_range():
     """Test generate_awk_script function on value range"""
-    fields = get_fields('./test_files/test.csv', 'file')
+    fields = get_fields('./test_files/test.csv', 'file', '|')
     match = parse_rules('./test_value_range.txt', fields)
     script_lines = generate_awk_script(match, fields).split('\n')
-    assert script_lines[
-        0] == 'BEGIN { FS="|"; print "ProductId|ProductPrice|ProductDescription|ProductCategory|" }'
-    assert script_lines[
-        1] == 'NR > 1 && $2 >= 2 && $2 < 5  { print $0 }'
+    assert script_lines[0] == 'BEGIN { FS="|"}'
+    assert script_lines[1] == 'NR == 1 { print $0 }'
+    assert script_lines[2] == 'NR > 1 && $2 >= 2 && $2 < 5  { print $0 }'
 
 
 def test_main_value_range(capsys):
@@ -165,8 +164,65 @@ def test_main_value_range(capsys):
     captured = capsys.readouterr().out.split('\n')
     print(captured)
     assert captured[0] == 'Results for ./test_files/test.csv'
-    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory|'
+    assert captured[1] == 'ProductId | ProductPrice | ProductDescription | ProductCategory'
     assert captured[2] == '1122 | 3.00 | plain crumpets | grocery'
     assert captured[3] == '1124| 2.00 | Salty jerky treads | pets'
     assert captured[4] == '1127 | 2.00 | peanut brittle | snacks'
     assert captured[5] == '1129 | 3.00 | peanuts ||'
+
+
+def test_main_no_header(capsys):
+    """Test !FIELDS directive."""
+    sys.argv = ['./greppy.py', './test_and_no_header.txt']
+    # Product description contains 'nut' and product category is not 'snacks'
+    main()
+    captured = capsys.readouterr().out.split('\n')
+    print(captured)
+    assert captured[0] == 'Results for test_files/no-header/test-no-header.csv'
+    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory'
+    assert captured[2] == '1126 | 1.00 | plain peanut butter | grocery'
+    assert captured[3] == '1128 || "peanut butter dog treats" | pets'
+    assert captured[4] == '1129 | 3.00 | peanuts ||'
+
+
+def test_in_comma_separated(capsys):
+    """Test !SEPARATOR directive."""
+    sys.argv = ['./greppy.py', './test_in_comma_sep.txt']
+    # Category not grocery or pets
+    main()
+    captured = capsys.readouterr().out.split('\n')
+    print(captured)
+    assert captured[0] == 'Results for test_files/test-comma-sep.csv'
+    assert captured[1] == 'ProductId , ProductPrice , ProductDescription , ProductCategory'
+    assert captured[2] == '1123 , 5.00 , salty dog pretzels , snacks'
+    assert captured[3] == '1125 , 1.00 , sugar crumpets , "snacks"'
+    assert captured[4] == '1127 , 2.00 , peanut brittle , snacks'
+    assert captured[5] == '1129 , 3.00 , peanuts ,,'
+
+
+def test_main_no_header_first_record_returned(capsys):
+    """Test !FIELDS directive."""
+    sys.argv = ['./greppy.py', './test_in_prices_no_header.txt']
+    # Product description contains 'nut' and product category is not 'snacks'
+    main()
+    captured = capsys.readouterr().out.split('\n')
+    print(captured)
+    assert captured[0] == 'Results for test_files/no-header/test-no-header.csv'
+    assert captured[1] == 'ProductId|ProductPrice|ProductDescription|ProductCategory'
+    assert captured[2] == '1121 | 7.50 | fancy crumpets | grocery'
+    assert captured[3] == '1123 | 5.00 | salty dog pretzels | snacks'
+
+
+"""
+1121 | 7.50 | fancy crumpets | grocery
+1122 | 3.00 | plain crumpets | grocery
+1123 | 5.00 | salty dog pretzels | snacks
+1124| 2.00 | Salty jerky treads | pets
+1125 | 1.00 | sugar crumpets | "snacks"
+1126 | 1.00 | plain peanut butter | grocery
+1127 | 2.00 | peanut brittle | snacks
+1128 || "peanut butter dog treats" | pets
+1129 | 3.00 | peanuts ||
+| | 1.00 | mystery cheeze | grocery
+11293 | 1.00 | mystery cheeze | grocery
+"""
